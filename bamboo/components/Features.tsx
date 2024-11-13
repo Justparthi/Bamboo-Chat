444b4017697aaa724797d28169d6fa791de696c0
justparthi <justparthiban@gmail.com>
2024-11-14 00:08:31 +0530

    Why Page

--- bamboo/components/Features.tsx
+++ bamboo/components/Features.tsx
@@ -3,19 +3,19 @@ import { Upload, Lock, Server } from 'lucide-react';
 
 const features = [
   {
-    name: 'Push to deploy.',
+    name: 'Simple & Intuitive',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      'No steep learning curves. Our interface is designed to be user-friendly, so you can start chatting right away.',
     icon: Upload,
   },
   {
-    name: 'SSL certificates.',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    name: 'Secure & Private',
+    description: 'We take your privacy seriously. With end-to-end encryption, your conversations are always protected.',
     icon: Lock,
   },
   {
     name: 'Database backups.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    description: 'Never worry about losing your conversations again! With Bamboo Chat, your chat history, files, and media are automatically backed up in real-time',
     icon: Server,
   },
 ];
@@ -27,13 +27,13 @@ export default function Example() {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base/7 font-semibold text-indigo-400">Deploy faster</h2>
+              <h2 className="text-base/7 font-semibold text-indigo-400">Where Ideas Meet.</h2>
               <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-200 sm:text-5xl">
-                A better workflow
+              Why Choose Bamboo Chat?
+
               </p>
               <p className="mt-6 text-lg/8 text-gray-400">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
+              From small groups to large teams, our app is built for high performance and reliability, ensuring smooth, uninterrupted communication no matter your size.
               </p>
               <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-400 lg:max-w-none">
                 {features.map((feature) => (
