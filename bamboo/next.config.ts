0e5d362a70e88617b8ff853e4ed130d98c70142b
Parthiban <155973403+Justparthi@users.noreply.github.com>
2024-12-24 21:42:38 +0530

    Update next.config.ts

--- bamboo/next.config.ts
+++ bamboo/next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-<<<<<<< HEAD
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
       },
@@ -14,23 +13,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'tailwindui.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',  // Add images.unsplash.com to the allowed domains
+        hostname: 'tailwindui.com',  // Allow images from tailwindui.com
       },
     ],
   },
-  // Add other configurations as needed
-=======
-        protocol: 'https',  // The protocol is 'https'
-        hostname: 'avatars.githubusercontent.com',  // The hostname of the remote image
-      },
-    ],
-  },
-  // Add any other config options you need here
->>>>>>> 7e07509a79e99daa898803e224d7e9c30fe51a98
+  // Any other configuration options can be added here
 };
 
 export default nextConfig;
